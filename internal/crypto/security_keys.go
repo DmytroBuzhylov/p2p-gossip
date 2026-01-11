101c86903758766bce3a6e0a09f01aeba1fc754a
kachegar <dmytro.buzhylov@gmail.com>
2026-01-11 17:56:58 +0200

    feat: protect private keys using Argon2id and ChaCha20

--- internal/crypto/security_keys.go
+++ internal/crypto/security_keys.go
@@ -8,23 +8,79 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/hex"
-	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"io"
 	"log"
 	"math/big"
-	"os"
 	"time"
 
 	"github.com/dgraph-io/badger/v4"
-	jsoniter "github.com/json-iterator/go"
+	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/chacha20poly1305"
+	"google.golang.org/protobuf/proto"
 )
 
-const ConfigPath = "node_identity.json"
+//type NodeKeys struct {
+//	PrivateKey string `json:"private_key"`
+//	PublicKey  string `json:"public_key"`
+//}
 
-type NodeKeys struct {
-	PrivateKey string `json:"private_key"`
-	PublicKey  string `json:"public_key"`
+type SecureKeyStore struct {
+	Password []byte
+	storage  storage.Storage
+}
+
+func NewSecureKeyStore(pass []byte, s storage.Storage) *SecureKeyStore {
+	return &SecureKeyStore{
+		Password: pass,
+		storage:  s,
+	}
+}
+
+func (s *SecureKeyStore) encryptPrivateKey(privKey ed25519.PrivateKey) ([]byte, error) {
+	salt := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return nil, err
+	}
+
+	key := argon2.IDKey(s.Password, salt, 1, 64*1024, 4, 32)
+
+	aead, _ := chacha20poly1305.NewX(key)
+	nonce := make([]byte, aead.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+
+	cipherText := aead.Seal(nil, nonce, privKey, nil)
+	protoKeys := &EncryptedPrivateKey{
+		Salt:         salt,
+		Nonce:        nonce,
+		EncryptedKey: cipherText,
+		Algorithm:    "argon2id-chacha20",
+	}
+	return proto.Marshal(protoKeys)
+}
+
+func (s *SecureKeyStore) decryptPrivateKey(encryptedData []byte) (ed25519.PrivateKey, error) {
+	var protoData EncryptedPrivateKey
+	err := proto.Unmarshal(encryptedData, &protoData)
+	if err != nil {
+		return nil, err
+	}
+
+	salt := protoData.GetSalt()
+	nonce := protoData.GetNonce()
+	actualCipher := protoData.GetEncryptedKey()
+
+	key := argon2.IDKey(s.Password, salt, 1, 64*1024, 4, 32)
+
+	aead, _ := chacha20poly1305.NewX(key)
+
+	decrypted, err := aead.Open(nil, nonce, actualCipher, nil)
+	if err != nil {
+		return nil, errors.New("decryption error (wrong password?)")
+	}
+
+	return ed25519.PrivateKey(decrypted), nil
 }
 
 func GetPublicKey(privKey ed25519.PrivateKey) ed25519.PublicKey {
@@ -34,28 +90,22 @@ func GetPublicKey(privKey ed25519.PrivateKey) ed25519.PublicKey {
 }
 
 // GetOrGenerateKeys loads keys from a file or creates new ones
-func GetOrGenerateKeys(s storage.Storage) (ed25519.PrivateKey, error) {
-	var (
-		keys NodeKeys
-		json = jsoniter.ConfigCompatibleWithStandardLibrary
-	)
+func (s *SecureKeyStore) GetOrGenerateKeys() (ed25519.PrivateKey, error) {
 
-	keysBytes, err := s.Get([]byte("secret:keys"))
+	keysBytes, err := s.storage.Get([]byte("secret:keys"))
 	if err != nil {
 		if errors.Is(err, badger.ErrKeyNotFound) {
-			pub, priv, err := ed25519.GenerateKey(rand.Reader)
+			_, priv, err := ed25519.GenerateKey(rand.Reader)
 			if err != nil {
 				return nil, err
 			}
 
-			keys.PublicKey, keys.PrivateKey = hex.EncodeToString(pub), hex.EncodeToString(priv)
-
-			keysBytes, err = json.Marshal(&keys)
+			keyData, err := s.encryptPrivateKey(priv)
 			if err != nil {
 				return nil, err
 			}
 
-			err = s.Set([]byte("secret:keys"), keysBytes)
+			err = s.storage.Set([]byte("secret:keys"), keyData)
 			if err != nil {
 				return nil, err
 			}
@@ -67,17 +117,7 @@ func GetOrGenerateKeys(s storage.Storage) (ed25519.PrivateKey, error) {
 		}
 	}
 
-	err = json.Unmarshal(keysBytes, &keys)
-	if err != nil {
-		return nil, err
-	}
-
-	if err == nil && keys.PrivateKey != "" && keys.PublicKey != "" {
-		return decodeKey(keys.PrivateKey)
-	} else {
-		return nil, err
-	}
-
+	return s.decryptPrivateKey(keysBytes)
 }
 
 // GenerateTLSConfig creates a config for QUIC/TLS based on the Identity key
@@ -127,27 +167,6 @@ func GenerateTLSConfig(priv ed25519.PrivateKey) (*tls.Config, error) {
 		},
 		ClientSessionCache: tls.NewLRUClientSessionCache(100),
 	}, nil
-}
-
-func loadConfig() (*NodeKeys, error) {
-	data, err := os.ReadFile(ConfigPath)
-	if err != nil {
-		return nil, err
-	}
-	var keys NodeKeys
-	if err := json.Unmarshal(data, &keys); err != nil {
-		return nil, err
-	}
-	return &keys, nil
-}
-
-func saveConfig(pub, priv string) error {
-	keys := NodeKeys{PublicKey: pub, PrivateKey: priv}
-	data, err := json.MarshalIndent(keys, "", "  ")
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(ConfigPath, data, 0600)
 }
 
 func decodeKey(hexStr string) (ed25519.PrivateKey, error) {
